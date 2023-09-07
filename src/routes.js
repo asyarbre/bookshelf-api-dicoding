3aa087a5f58035652a03f908f6dd1006fa92a7f5
Asya'ri <asyari818@gmail.com>
2023-09-07 20:24:56 +0700

    feat: create book

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
-const routes = [
+const createBook = require("./handler/createBook");
+const getAllBook = require("./handler/getAllBook");
 
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: createBook,
+  },
 ];
 
 module.exports = routes;
